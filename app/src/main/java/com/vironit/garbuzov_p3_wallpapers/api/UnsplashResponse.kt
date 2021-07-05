0a1b0c9958d4ad7007a9198f5b88df94231230c7
Boris Garbuzov <b.garbuzov.vironit@vironit.ru>
2021-07-05 10:52:16 +0300

    added dagger and retrofit dependencies, unsplash api and unsplash access key

---
+++ app/src/main/java/com/vironit/garbuzov_p3_wallpapers/api/UnsplashResponse.kt
@@ -0,0 +1,7 @@
+package com.vironit.garbuzov_p3_wallpapers.api
+
+import com.vironit.garbuzov_p3_wallpapers.data.Photo
+
+data class UnsplashResponse(
+    val results: List<Photo>
+)
--- app/src/main/java/com/vironit/garbuzov_p3_wallpapers/data/Photo.kt
+++ app/src/main/java/com/vironit/garbuzov_p3_wallpapers/data/Photo.kt
@@ -17,4 +17,48 @@ data class Photo(
     @ColumnInfo(name = "user") val user: User,
     @ColumnInfo(name = "urls") val urls: Urls,
     @ColumnInfo(name = "links") val links: Links
-)
+){
+    @Entity(tableName = "user")
+    data class User(
+        @PrimaryKey val userId: String,
+        @ColumnInfo(name = "username") val username: String,
+        @ColumnInfo(name = "name") val name: String,
+        @ColumnInfo(name = "first_name") val firstName: String,
+        @ColumnInfo(name = "last_name") val lastName: String,
+        @ColumnInfo(name = "instagram_username") val instagramUsername: String,
+        @ColumnInfo(name = "twitter_username") val twitterUsername: String,
+        @ColumnInfo(name = "portfolio_url") val portfolioUrl: String,
+        @ColumnInfo(name = "profile_image") val profileImage: ProfileImage,
+        @ColumnInfo(name = "links") val links: Links
+    ){
+        @Entity(tableName = "profile_image")
+        data class ProfileImage(
+            @ColumnInfo(name = "small") val small: String,
+            @ColumnInfo(name = "medium") val medium: String,
+            @ColumnInfo(name = "large") val large: String
+        )
+
+        @Entity(tableName = "links")
+        data class Links(
+            @ColumnInfo(name = "self") val self: String,
+            @ColumnInfo(name = "html") val html: String,
+            @ColumnInfo(name = "photos") val photos: String,
+            @ColumnInfo(name = "likes") val likes: String
+        )
+    }
+    @Entity(tableName = "urls")
+    data class Urls(
+        @ColumnInfo(name = "raw") val raw: String,
+        @ColumnInfo(name = "full") val full: String,
+        @ColumnInfo(name = "regular") val regular: String,
+        @ColumnInfo(name = "small") val small: String,
+        @ColumnInfo(name = "thumb") val thumb: String
+    )
+    @Entity(tableName = "links")
+    data class Links(
+        @ColumnInfo(name = "self") val self: String,
+        @ColumnInfo(name = "html") val html: String,
+        @ColumnInfo(name = "photos") val photos: String,
+        @ColumnInfo(name = "likes") val likes: String
+    )
+}
