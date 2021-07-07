a7dcce9c2e159d172965645378dd495b69fa2863
Boris Garbuzov <b.garbuzov.vironit@vironit.ru>
2021-07-07 11:02:24 +0300

    added toolbar with searchview

--- app/src/main/java/com/vironit/garbuzov_p3_wallpapers/ui/adapters/PhotosSearchAdapter.kt
+++ app/src/main/java/com/vironit/garbuzov_p3_wallpapers/ui/adapters/PhotosSearchAdapter.kt
@@ -37,7 +37,7 @@ class PhotosSearchAdapter : PagingDataAdapter<Photo, PhotosSearchAdapter.PhotosH
             binding.apply {
                 Glide.with(itemView)
                     .load(photo.urls.regular)
-                    .fitCenter()
+                    .centerInside()
                     .transition(DrawableTransitionOptions.withCrossFade())
                     .error(R.drawable.ic_error)
                     .into(currentPhoto)
